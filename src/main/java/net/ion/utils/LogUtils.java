91b2d6d97a3acb67270f95f26f528b051f8d5763
Lee MinHo <mkeasy@gmail.com>
2017-02-13 15:29:17 +0900

    save temporary

--- src/main/java/net/ion/utils/LogUtils.java
+++ src/main/java/net/ion/utils/LogUtils.java
@@ -5,7 +5,8 @@ import org.apache.commons.logging.LogFactory;
 
 // 로그 사용방식을 통일하기 위해서 .... 
 // protected Log logger = LogFactory.getLog(this.getClass());
-public class LogUtils {
+// 음 ... 이걸 어떻게 써먹어야 하지?
+public abstract class LogUtils {
 	static Log getLog(Class<?> clazz) {
 		return LogFactory.getLog(clazz);
 	}
