09372c26799c051992ade43cdd6533be69698920
Lee MinHo <mkeasy@gmail.com>
2017-02-23 11:58:16 +0900

    separate es repository server

--- src/main/java/net/ion/advice/ExceptionAdvice.java
+++ src/main/java/net/ion/advice/ExceptionAdvice.java
@@ -1,4 +1,4 @@
-package net.ion.controller;
+package net.ion.advice;
 
 import java.nio.file.AccessDeniedException;
 
--- src/main/java/net/ion/system/ElasticsearchConfiguration.java
+++ src/main/java/net/ion/system/ElasticsearchConfiguration.java
@@ -4,13 +4,18 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.node.NodeBuilder;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,7 +48,9 @@ public class ElasticsearchConfiguration implements DisposableBean {
 
 	@Bean
 	public Client esClient() {
-
+		/* 주의 : 버전은 2.4.4 로 통일되어야 한다. */
+		/* Embedding ElasticSearch 사용시 */
+		/*
 		final String clusterName = properties.getClusterName();
 
 		Map<String, String> props = properties.getProperties();
@@ -61,6 +68,16 @@ public class ElasticsearchConfiguration implements DisposableBean {
 				.clusterName(clusterName)
 				.node()
 				.client();
+		*/	
+		/* Remote ElasticSearch 사용시 */
+        Client client = null;
+		try {
+			client = TransportClient.builder().build()
+				.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"),9300));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+        return client;
 	}
 
     @Override
